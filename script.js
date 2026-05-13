71810e02010b70b0756a7cd5adeb748032c4540d
Junior Knoth <juniorknoth22@gmail.com>
2026-05-13 16:02:13 -0300

    feat: fixed last_access feature

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // URL gerada ao implantar gas/Codigo.gs como App da Web no Google Apps Script
 // Ver instruções em gas/Codigo.gs
 const GAS_URL =
-  "https://script.google.com/macros/s/AKfycbzrqGSjjs9DqPb49kgJ2wk2Utp1ruz_PyKMMRHUNe18AYyiPB2oKnn-xluJfyuTjVCv/exec";
+  "https://script.google.com/macros/s/AKfycbyFRh6mPanPQOWAXx0feolH9ToI6b_AFdhFmksvZb06OojihXKw8NUaXonnmAB9gEMK/exec";
 
 // Chave gratuita do ImgBB — obter em: https://api.imgbb.com
 // Criar conta, gerar chave API e colar aqui
